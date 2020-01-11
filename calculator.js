5bbe362e09789435d0d22d5b2153435b78f84328
lucas-kitzmueller <mail@lucaskitzmueller.eu>
2020-01-11 17:03:20 -0500

    adding examples

--- calculator.js
+++ calculator.js
@@ -16,3 +16,21 @@ function subtract(a,b) {
     return a - b
 }
 
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
+
+console.log("I'm going use the calculator functions to add 5 and 6")
+var x = add(5,6)
+console.log(x)
+
+console.log("I'm going use the calculator functions to subtract 5 from 6")
+var x = subtract(6,5)
+console.log(x)
+
+console.log("I'm going use the calculator functions to divide 6 from 5")
+var x = divide(6,5)
+console.log(x)
+
+
+
