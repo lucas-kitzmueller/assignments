ef51a7f1080e0b6a4023c0346ba554a2712ee1f0
lucas-kitzmueller <mail@lucaskitzmueller.eu>
2020-01-11 16:35:38 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# Write a program that defines four functions (multiply, add, subtract, 
+# and divide). These functions should not print anything, they should 
+# simply perform a mathematical operation on the two arguments and return 
+# the value.
+
+def multiply(a,b):
+    return a * b
+
+def add(a.b):
+	return a + b
+
+def subtract(a,b):
+	return a - b
+
+def divide(a,b): 
+	return a//b
+
