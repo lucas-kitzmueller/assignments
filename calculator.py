55d29ebb7883c3e87a34018777662edd5feaf54b
lucas-kitzmueller <mail@lucaskitzmueller.eu>
2020-01-11 16:47:08 -0500

    adding bonus

--- calculator.py
+++ calculator.py
@@ -13,8 +13,21 @@ def subtract(a,b):
 	return a - b
 
 def divide(a,b): 
-	return a//b
+	return a/b
 
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 print(x)
+
+
+def square(a):
+	return a**2
+
+def cube(a):
+	return a**3
+
+def square_n_times(a,b)
+	return a**b
+
+
+
