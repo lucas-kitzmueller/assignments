e2daf4f707c8860c89be53c02ff2fd5eea6b6d7a
lucas-kitzmueller <mail@lucaskitzmueller.eu>
2020-01-18 13:34:18 -0500

    adding more text to expensive_water_desciption.txt

--- homework_1.sh
+++ homework_1.sh
@@ -19,9 +19,13 @@ cat expensive_water.csv | csvstat > expensive_water_summary.txt
 # open a file called expensive_water_description.txt. In your own words, in a few sentences, explain what you've just done.
 echo "First, I downloaded a dataset that seems to list the expenditures of members of the House of Representatives 
 for their Washington and and district offices in the first quarter of 2017. (https://projects.propublica.org/represent/expenditures). 
-Second, I filtered all expenses with the purpose water and an amount above 1000 using the python script.
-Third, I created summary statistics using csvstat.
-(also use terminology, like piping or )" > expensive_water_description.txt
+Second, I filtered all expenses with the purpose water and an amount above 1000 using the python script. 
+For doing so, I piped stdout from curl as stdin into the python file, where I assigned it the variable data. 
+Then, I used the csv libarary to create a csv reader and a writer. Then, I looped through all rows and if the 
+condition that the purpose is water and the amount is above 1000 was met, I added the row. The resulting csv 
+became stdout was redirected to expensive_water.csv.
+Third, I created summary statistics using csvstat. I did this by piping expensive_water.csv into csvstat and
+then redirecting the output into expensive_water_summary.txt"> expensive_water_description.txt
 
 # Done
 echo "Homework 1 - Part 1: Done"
